c2ecf3529b23cff1de420d2e625d89968ba587df
sandyclaws89 <96080101+sandyclaws89@users.noreply.github.com>
2022-02-23 18:47:46 +0100

    Finito controllo in console

--- script/js.js
+++ script/js.js
@@ -23,29 +23,23 @@ let insideBox = document.querySelector(`.box-container`);
 insideBox=0;
 // let=0;
 
-    for (let i=1; i<=100; ++i){
+    for (var i=1; i<=100; i++){
     
         if (i % 3 == 0) {
-            insideBox.innerHTML = insideBox.innerHTML + `<div class="box">Buzz<div>`
-        } else {
+            insideBox.innerHTML = insideBox.innerHTML + `<div class="box">Buzz<div>`;
+        } else if (i % 5 ==0){
+            insideBox.innerHTML = insideBox.innerHTML + `<div class="box">Fizz<div>`;
+        } else if (i % 15 ==0){
+            insideBox.innerHTML = insideBox.innerHTML + `<div class="box">FizzBuzz<div>`;
+        }
+        else {
             insideBox.innerHTML = insideBox.innerHTML + `<div class="box">${i}<div>`;
-        } 
-
-        if (i % 5 ==0) {
-            insideBox.innerHTML = insideBox.innerHTML + `<div class="box">Fizz<div>`
-        } else {
-            insideBox.innerHTML = insideBox.innerHTML + `<div class="box">${i}<div>`
-        } 
-
-        if (i % 15 ==0){
-            insideBox.innerHTML = insideBox.innerHTML + `<div class="box">FizzBuzz<div>`
-        } else {
-            insideBox.innerHTML = insideBox.innerHTML + `<div class="box">${i}<div>`
+            // console.log(i);
         }
 
-        // console.log(i);
+        console.log(i);
         }
-        
+       
 
 
     
