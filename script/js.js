978ac80ab2857bfe17e902c933d413d68f2f8e31
sandyclaws89 <96080101+sandyclaws89@users.noreply.github.com>
2022-02-23 19:05:57 +0100

    Non riesco a stampare i div nell'html

--- script/js.js
+++ script/js.js
@@ -21,33 +21,37 @@
 // var i=0;
 let insideBox = document.querySelector(`.box-container`);
 insideBox=0;
-// let=0;
+
+                // insideBox.classList.add('box')
+
 
     for (var i=1; i<=100; ++i){
     
-        if (i % 15 ==0 && i % 3==0){
+        if (i % 15 ==0){
             insideBox.innerHTML = insideBox.innerHTML + `<div class="box">FizzBuzz<div>`;
+            // insideBox.classList.add('.box')
             console.log(`FizzBuzz`);
            
         } else if (i % 5 ==0){
             insideBox.innerHTML = insideBox.innerHTML + `<div class="box">Fizz<div>`;
             console.log(`Fizz`);
-        } else if  (i % 3 == 0) {
+        } else if  (i % 3 == 0){
             insideBox.innerHTML = insideBox.innerHTML + `<div class="box">Buzz<div>`;
             console.log(`Buzz`);
         }
         else {
             insideBox.innerHTML = insideBox.innerHTML + `<div class="box">${i}<div>`;
-            // console.log(i);
-        }
             console.log(i);
         }
+            // console.log(i);
+        }
+
+    // insideBox.classList.add('box')
+
+    // CREDEVO CHE I DIV FOSSERO COLLEGATI ALL'HTML CON INNERHTML, HO PROVATO ANCHE AD AGGIUNGERE LA CLASSE CHE HO CREATO MA MI DA ERRORE. NON RIESCO A FAR STAMPARE I DIV IN HTML
+
        
-        // console.log(i);
 
 
-    
-
-    // HO PROVATO A SCRIVERE QUESTO MA MI DA ERRORE NELL'ELSE FINALE
 
 
