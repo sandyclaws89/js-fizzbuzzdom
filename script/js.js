996717b38c48ab0e9e05defb0285b7b89f30dff8
sandyclaws89 <96080101+sandyclaws89@users.noreply.github.com>
2022-02-23 17:40:02 +0100

    Piccole correzioni ma ancora non funziona, da rivedere la logica

--- script/js.js
+++ script/js.js
@@ -6,27 +6,29 @@
 
 // console.log(ciao);
 
-let insideBox = document.querySelector(`box-containers`);
-let=0;
 
 // for (let i=1; i<=100; ++i){
 //     if (i % 3 == 0) {
 //         insideBox.innerHTML = insideBox.innerHTML + `<div class=box>Buzz<div>`
 //     } else if (i % 5 ==0) {
-//         insideBox.innerHTML = insideBox.innerHTML + `<div class=box>Fizz<div>`
-//     } else if (i % 15 ==0){
-//         insideBox.innerHTML = insideBox.innerHTML + `<div class=box>FizzBuzz<div>`
-//     } else {
-//         insideBox.innerHTML = insideBox.innerHTML + `<div class=box>${i}<div>`
+    //         insideBox.innerHTML = insideBox.innerHTML + `<div class=box>Fizz<div>`
+    //     } else if (i % 15 ==0){
+        //         insideBox.innerHTML = insideBox.innerHTML + `<div class=box>FizzBuzz<div>`
+        //     } else {
+            //         insideBox.innerHTML = insideBox.innerHTML + `<div class=box>${i}<div>`
 //     }
 //     }
+// var i=0;
+let insideBox = document.querySelector(`.box-container`);
+insideBox=0;
+// let=0;
 
     for (let i=1; i<=100; ++i){
     
         if (i % 3 == 0) {
             insideBox.innerHTML = insideBox.innerHTML + `<div class="box">Buzz<div>`
         } else {
-            insideBox.innerHTML = insideBox.innerHTML + `<div class="box">${i}<div>`
+            insideBox.innerHTML = insideBox.innerHTML + `<div class="box">${i}<div>`;
         } 
 
         if (i % 5 ==0) {
@@ -40,14 +42,13 @@ let=0;
         } else {
             insideBox.innerHTML = insideBox.innerHTML + `<div class="box">${i}<div>`
         }
+
+        // console.log(i);
         }
+        
 
 
-
-
-
-
-    console.log(i);
+    
 
     // HO PROVATO A SCRIVERE QUESTO MA MI DA ERRORE NELL'ELSE FINALE
 
