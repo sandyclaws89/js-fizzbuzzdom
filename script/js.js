6f073e1ebafd16efda9edbdc7a70a9b2f6d225a3
sandyclaws89 <96080101+sandyclaws89@users.noreply.github.com>
2022-02-23 17:13:28 +0100

    tentativo ma continua a darmi errore sulla $i dentro innerhtml

--- script/js.js
+++ script/js.js
@@ -9,17 +9,43 @@
 let insideBox = document.querySelector(`box-containers`);
 let=0;
 
-for (let i=1; i<=100; ++i){
-    if (i % 3 == 0) {
-        insideBox.innerHTML = insideBox.innerHTML + `<div class=box>Buzz<div>`
-    } else if (i % 5 ==0) {
-        insideBox.innerHTML = insideBox.innerHTML + `<div class=box>Fizz<div>`
-    } else if (i % 15 ==0){
-        insideBox.innerHTML = insideBox.innerHTML + `<div class=box>FizzBuzz<div>`
-    } else {
-        insideBox.innerHTML = insideBox.innerHTML + `<div class=box>${i}<div>`
-    }
-    }
+// for (let i=1; i<=100; ++i){
+//     if (i % 3 == 0) {
+//         insideBox.innerHTML = insideBox.innerHTML + `<div class=box>Buzz<div>`
+//     } else if (i % 5 ==0) {
+//         insideBox.innerHTML = insideBox.innerHTML + `<div class=box>Fizz<div>`
+//     } else if (i % 15 ==0){
+//         insideBox.innerHTML = insideBox.innerHTML + `<div class=box>FizzBuzz<div>`
+//     } else {
+//         insideBox.innerHTML = insideBox.innerHTML + `<div class=box>${i}<div>`
+//     }
+//     }
+
+    for (let i=1; i<=100; ++i){
+    
+        if (i % 3 == 0) {
+            insideBox.innerHTML = insideBox.innerHTML + `<div class="box">Buzz<div>`
+        } else {
+            insideBox.innerHTML = insideBox.innerHTML + `<div class="box">${i}<div>`
+        } 
+
+        if (i % 5 ==0) {
+            insideBox.innerHTML = insideBox.innerHTML + `<div class="box">Fizz<div>`
+        } else {
+            insideBox.innerHTML = insideBox.innerHTML + `<div class="box">${i}<div>`
+        } 
+
+        if (i % 15 ==0){
+            insideBox.innerHTML = insideBox.innerHTML + `<div class="box">FizzBuzz<div>`
+        } else {
+            insideBox.innerHTML = insideBox.innerHTML + `<div class="box">${i}<div>`
+        }
+        }
+
+
+
+
+
 
     console.log(i);
 
